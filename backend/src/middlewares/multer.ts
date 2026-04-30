74a4f17e319b726674a6385feddc55469cb3899b
ulyana <novikova.ula.007@gmail.com>
2026-04-30 20:38:16 +0300

    refactor(backend): use factory function for multer storage and add cocktails path

--- backend/src/middlewares/multer.ts
+++ backend/src/middlewares/multer.ts
@@ -4,17 +4,23 @@ import config from '../config.ts';
 import fs from 'fs/promises';
 import { randomUUID } from 'crypto';
 
-const imageStorage = multer.diskStorage({
-  destination: async (_req, __file, cb) => {
-    const destDir = path.join(config.publicPath, 'images');
-    await fs.mkdir(destDir, { recursive: true });
-    cb(null, destDir);
-  },
+const createStorage = (subDir: string) => {
+  return multer.diskStorage({
+    destination: async (_req, _file, cb) => {
+      const destDir = path.join(config.publicPath, subDir);
+      await fs.mkdir(destDir, { recursive: true });
+      cb(null, destDir);
+    },
+    filename: (_req, file, cb) => {
+      const extension = path.extname(file.originalname);
+      cb(null, randomUUID() + extension);
+    },
+  });
+};
 
-  filename: (_req, file, cb) => {
-    const extension = path.extname(file.originalname);
-    cb(null, randomUUID() + extension);
-  },
+export const imagesUpload = multer({
+  storage: createStorage('images'),
 });
-
-export const imagesUpload = multer({ storage: imageStorage });
+export const cocktailUpload = multer({
+  storage: createStorage('uploads/cocktails'),
+});
