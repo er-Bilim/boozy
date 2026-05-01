910e62b146b2859cd2c4a68e2d1d243b58f4552e
ulyana <novikova.ula.007@gmail.com>
2026-05-01 17:22:19 +0300

    feat(frontend): extract cocktail card to separate component with admin actions

--- frontend/src/pages/CocktailsPage/CocktailsPage.tsx
+++ frontend/src/pages/CocktailsPage/CocktailsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '@/app/hooks.ts';
 import {
   selectCocktails,
@@ -8,17 +8,12 @@ import {
 import { fetchCocktails } from '@/features/cocktail/cocktailThunks.ts';
 import { toast } from 'react-toastify';
 import CocktailSkeleton from '@/components/CocktailSkeleton/CocktailSkeleton.tsx';
+import CocktailCard from '@/components/CocktailCard/CocktailCard.tsx';
 
 const CocktailsPage = () => {
   const dispatch = useAppDispatch();
   const cocktails = useAppSelector(selectCocktails);
   const loading = useAppSelector(selectLoading).fetchLoading;
-
-  const getImage = (image: string | null) => {
-    if (!image) return 'https://via.placeholder.com/300';
-    if (image.startsWith('http')) return image;
-    return `http://localhost:8000/${image}`;
-  };
 
   const fetchAllCocktails = async () => {
     try {
@@ -42,26 +37,16 @@ const CocktailsPage = () => {
         {loading ? (
           <CocktailSkeleton />
         ) : (
-          cocktails
-            .filter((c) => c.isPublished)
-            .map((cocktail) => (
-              <Card key={cocktail._id} sx={{ width: 300 }}>
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={getImage(cocktail.image)}
-                  alt={cocktail.title}
-                />
-
-                <CardContent>
-                  <Typography variant="h6">{cocktail.title}</Typography>
-
-                  <Typography variant="body2" sx={{ mt: 1 }}>
-                    {cocktail.recipe}
-                  </Typography>
-                </CardContent>
-              </Card>
-            ))
+          cocktails.map((cocktail) => (
+            <CocktailCard
+              key={cocktail._id}
+              isPublished={cocktail.isPublished}
+              title={cocktail.title}
+              recipe={cocktail.recipe}
+              image={cocktail.image}
+              id={cocktail._id}
+            />
+          ))
         )}
       </Box>
     </Box>
